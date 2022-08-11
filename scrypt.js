77389edfdce2fae6930c484792a19193b5378c2b
Elian_Isaac <96488197+Lin-0103@users.noreply.github.com>
2022-08-11 12:30:42 -0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,4 @@
+var novolink = document.querySelector('#imagem')
+function mudarimagem() {
+  novolink.setAttribute('src', 'https://scontent.fvcp2-1.fna.fbcdn.net/v/t1.18169-9/22687650_770639579790908_2910801572220195004_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=4BVaUpr6MGwAX9t0wmj&_nc_ht=scontent.fvcp2-1.fna&oh=9bd96eaf44f712a56122431d1a9effd1&oe=60EDE789')
+}
